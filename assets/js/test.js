32f124d74589210da7e7ddf0625e432b13268a87
Juliette <juliettesalevsky@gmail.com>
2023-03-11 04:01:02 +0100

    start

---
+++ assets/js/test.js
@@ -0,0 +1,9 @@
+require('../scss/project.scss');
+
+class Test {
+    static onReady() {
+        // put your js methods here
+    }
+}
+
+document.addEventListener('DOMContentLoaded', Test.onReady);
